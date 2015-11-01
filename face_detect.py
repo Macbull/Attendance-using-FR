95609298761add63c8dfedfac4c3f4e11a83ea01
Vishwajeet Narwal <vnarwal95@gmail.com>
2015-11-02 02:43:04 +0530

    showing lpb of faces

--- face_detect.py
+++ face_detect.py
@@ -1,9 +1,10 @@
 import sys
+from skimage.feature import local_binary_pattern
 
 sys.path.append('/usr/local/lib/python2.7/site-packages')
 
 import cv2
-import sys
+
 
 # Get user supplied values
 imagePath = sys.argv[1]
@@ -26,10 +27,23 @@ faces = faceCascade.detectMultiScale(
 )
 
 print "Found {0} faces!".format(len(faces))
-
+faceImages = [];
 # Draw a rectangle around the faces
 for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2);
+    faceImages.append(image[y:y+h,x:x+h]);
 
-cv2.imshow("Faces found", image)
-cv2.waitKey(0)
+cv2.imshow("Faces found", image);
+cv2.waitKey(0);
+radius = 3
+no_points = 8 * radius
+featureImage=[];
+for face in faceImages:
+	cv2.imshow("face", face);
+	cv2.waitKey(0);
+	im_gray = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+	featureImage.append(local_binary_pattern(im_gray, no_points, radius, method='ror'))
+	
+for face in featureImage:
+	cv2.imshow("features",face)
+	cv2.waitKey(0);
